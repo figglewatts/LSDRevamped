9ea37a5b4f0ad9d965f808fc604b1c9887da840c
Figglewatts <figglewatts@hotmail.co.uk>
2016-07-11 18:54:02 +0100

    Implemented a script that manages the main game loop and initialization

---
+++ LSDR/Assets/Scripts/Game/GameScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using InputManagement;
+
+public class GameScript : MonoBehaviour
+{
+	void Start()
+	{
+		// do game startup stuff here
+
+		// TODO: load current input scheme from game settings
+		// TODO: if no settings are found load a default input scheme
+		InputHandler.AddInput("Forward", KeyCode.W);
+		InputHandler.AddInput("Backward", KeyCode.S);
+		InputHandler.AddInput("Left", KeyCode.A);
+		InputHandler.AddInput("Right", KeyCode.D);
+
+
+		InputHandler.Initialize();
+	}
+
+	void Update()
+	{
+		// Do game update stuff here
+		InputHandler.HandleInput();
+	}
+}
