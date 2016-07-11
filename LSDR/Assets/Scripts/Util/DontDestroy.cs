35fc9099fb1179e395f13766c449cb7ec6b95eff
Figglewatts <figglewatts@hotmail.co.uk>
2016-07-11 18:53:27 +0100

    Implemented DontDestroy

---
+++ LSDR/Assets/Scripts/Util/DontDestroy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+	public class DontDestroy : MonoBehaviour
+	{
+		void Awake()
+		{
+			// make sure there isn't another one
+			GameObject obj = GameObject.Find(this.gameObject.name);
+			if (!obj){ return; }
+
+			int myId = this.gameObject.GetInstanceID();
+			int otherId = obj.GetInstanceID();
+
+			if (myId == otherId){ return; }
+			
+			if (myId > otherId)
+			{
+				DestroyImmediate(this.gameObject);
+			}
+		}
+
+		void Start()
+		{
+			DontDestroyOnLoad(this.gameObject);
+		}
+	}
+}
