95116a50208c57cf107b153c0ce5de05bd34bf3c
Figglewatts <figglewatts@hotmail.co.uk>
2019-11-15 12:25:45 +0000

    Fixed copy-nuget-packages.sh to create dir

--- copy-nuget-packages.sh
+++ copy-nuget-packages.sh
@@ -6,6 +6,8 @@ packages=( "Newtonsoft.Json.12.0.1/lib/net45/Newtonsoft.Json.dll"
     "SharpZipLib.1.1.0/lib/net45/ICSharpCode.SharpZipLib.dll" )
 outputPath="LSDR/Assets/UnityEditor/Dep/External/"
 
+mkdir -p "$outputPath"
+
 for package in "${packages[@]}"
 do
    cp "$packagesPath/$package" "$outputPath"
