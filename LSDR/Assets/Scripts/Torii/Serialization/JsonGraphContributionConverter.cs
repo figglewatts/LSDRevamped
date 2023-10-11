6d60efbf4a55455ab7e289cdf7868caac3ff39cb
Figglewatts <me@figglewatts.co.uk>
2023-10-11 20:34:20 +0100

    Add playerposition/rotation to graph contribution (for flashbacks)

--- LSDR/Assets/Scripts/Torii/Serialization/JsonGraphContributionConverter.cs
+++ LSDR/Assets/Scripts/Torii/Serialization/JsonGraphContributionConverter.cs
@@ -2,27 +2,56 @@
 using LSDR.SDK.Data;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 namespace Torii.Serialization
 {
     public class JsonGraphContributionConverter : JsonConverter<GraphContribution>
     {
-        public override void WriteJson(JsonWriter writer, GraphContribution contribution, JsonSerializer serializer)
+        public override void WriteJson(JsonWriter writer, GraphContribution value, JsonSerializer serializer)
         {
-            writer.WriteStartArray();
-            writer.WriteValue(contribution.Dynamic);
-            writer.WriteValue(contribution.Upper);
-            writer.WriteEndArray();
+            JObject obj = new JObject
+            {
+                { "contribution", new JArray(value.Dynamic, value.Upper) },
+                { "position", new JArray(value.PlayerPosition.x, value.PlayerPosition.y, value.PlayerPosition.z) },
+                { "yRotation", value.PlayerYRotation }
+            };
+            obj.WriteTo(writer);
         }
 
-        public override GraphContribution ReadJson(JsonReader reader,
-            Type objectType,
-            GraphContribution existingValue,
+        public override GraphContribution ReadJson(JsonReader reader, Type objectType, GraphContribution existingValue,
             bool hasExistingValue,
             JsonSerializer serializer)
         {
-            JArray arr = JArray.Load(reader);
-            return new GraphContribution(arr[index: 0].ToObject<int>(), arr[index: 1].ToObject<int>());
+            try
+            {
+                JObject obj = JObject.Load(reader);
+
+                if (!obj.ContainsKey("contribution"))
+                {
+                    throw new JsonSerializationException("missing required key 'contribution' in object");
+                }
+
+                var contributionArray = obj["contribution"]!.ToObject<int[]>();
+                float[] positionArray = obj.ContainsKey("position")
+                    ? obj["position"]!.ToObject<float[]>()
+                    : new float[] { 0, 0, 0 };
+                float yRotation = obj.ContainsKey("yRotation") ? obj["yRotation"]!.ToObject<float>() : 0f;
+
+                Vector3 position = new Vector3(positionArray[0], positionArray[1], positionArray[2]);
+
+                GraphContribution graphContribution =
+                    new GraphContribution(contributionArray[0], contributionArray[1], position, yRotation);
+
+                return graphContribution;
+            }
+            catch (JsonReaderException)
+            {
+                // if the object was invalid it's very likely to be data in the old format, so try loading it like that
+                // i.e. without player position and rotation
+                JArray arr = JArray.Load(reader);
+                return new GraphContribution(arr[index: 0].ToObject<int>(), arr[index: 1].ToObject<int>());
+            }
         }
     }
 }
--- LSDR/Packages/com.figglewatts.lsdr.sdk/Runtime/Data/GraphContribution.cs
+++ LSDR/Packages/com.figglewatts.lsdr.sdk/Runtime/Data/GraphContribution.cs
@@ -11,18 +11,35 @@ namespace LSDR.SDK.Data
     public struct GraphContribution
     {
         [SerializeField] private Vector2Int _vector;
+        private Vector3 _playerPosition;
+        private float _playerYRotation;
 
         public GraphContribution(int dynamicness, int upperness)
         {
             _vector = new Vector2Int(dynamicness, upperness);
+            _playerPosition = Vector3.zero;
+            _playerYRotation = 0;
+        }
+
+        public GraphContribution(int dynamicness, int upperness, Transform playerTransform)
+            : this(dynamicness, upperness)
+        {
+            _playerPosition = playerTransform.position;
+            _playerYRotation = playerTransform.eulerAngles.y;
+        }
+
+        public GraphContribution(int dynamicness, int upperness, Vector3 playerPosition, float playerYRotation)
+            : this(dynamicness, upperness)
+        {
+            _playerPosition = playerPosition;
+            _playerYRotation = playerYRotation;
         }
 
         public int Dynamic => _vector.x;
         public int Upper => _vector.y;
 
-        public static implicit operator Vector2Int(GraphContribution gc)
-        {
-            return gc._vector;
-        }
+        public Vector3 PlayerPosition => _playerPosition;
+
+        public float PlayerYRotation => _playerYRotation;
     }
 }
