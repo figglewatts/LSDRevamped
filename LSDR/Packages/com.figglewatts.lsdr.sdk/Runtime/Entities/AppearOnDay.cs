d19afad50cd0724c0cbe24e9dadbf859515832e7
Figglewatts <me@figglewatts.co.uk>
2024-05-13 18:28:26 +0100

    Add AppearOnDay

---
+++ LSDR/Packages/com.figglewatts.lsdr.sdk/Runtime/Entities/AppearOnDay.cs
@@ -0,0 +1,77 @@
+﻿using LSDR.SDK.DreamControl;
+using UnityEngine;
+
+namespace LSDR.SDK.Entities
+{
+    public class AppearOnDay : MonoBehaviour
+    {
+        public DayKind Day;
+
+        public void Start()
+        {
+            var dreamSystem = DreamControlManager.Managed;
+            var dayNumber = dreamSystem.CurrentDay;
+
+            gameObject.SetActive(false);
+            if (checkDayKind(dayNumber)) gameObject.SetActive(true);
+        }
+
+        protected bool checkDayKind(int dayNumber)
+        {
+            bool result = false;
+            if (Day == DayKind.None) return false;
+
+            if (Day.HasFlag(DayKind.Even))
+            {
+                result |= dayNumber % 2 == 0;
+            }
+
+            if (Day.HasFlag(DayKind.Odd))
+            {
+                result |= dayNumber % 2 != 0;
+            }
+
+            bool dayIsNumber(int dayNumber, int weekIndex)
+            {
+                return ((dayNumber - 1) % 7) + 1 == weekIndex;
+            }
+
+            if (Day.HasFlag(DayKind.One))
+            {
+                result |= dayIsNumber(dayNumber, 1);
+            }
+
+            if (Day.HasFlag(DayKind.Two))
+            {
+                result |= dayIsNumber(dayNumber, 2);
+            }
+
+            if (Day.HasFlag(DayKind.Three))
+            {
+                result |= dayIsNumber(dayNumber, 3);
+            }
+
+            if (Day.HasFlag(DayKind.Four))
+            {
+                result |= dayIsNumber(dayNumber, 4);
+            }
+
+            if (Day.HasFlag(DayKind.Five))
+            {
+                result |= dayIsNumber(dayNumber, 5);
+            }
+
+            if (Day.HasFlag(DayKind.Six))
+            {
+                result |= dayIsNumber(dayNumber, 6);
+            }
+
+            if (Day.HasFlag(DayKind.Seven))
+            {
+                result |= dayIsNumber(dayNumber, 7);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LSDR/Packages/com.figglewatts.lsdr.sdk/Runtime/Entities/DayKind.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LSDR.SDK.Entities
+{
+    [Flags]
+    public enum DayKind
+    {
+        None = 0,
+
+        Odd = 1 << 1,
+        Even = 1 << 2,
+        One = 1 << 3,
+        Two = 1 << 4,
+        Three = 1 << 5,
+        Four = 1 << 6,
+        Five = 1 << 7,
+        Six = 1 << 8,
+        Seven = 1 << 9,
+
+        All = Odd | Even | One | Two | Three | Four | Five | Six | Seven
+    }
+}
