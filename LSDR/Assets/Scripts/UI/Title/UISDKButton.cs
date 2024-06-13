a5e3601c48fcbb0930fe7deac8d67be1efa866fa
Figglewatts <me@figglewatts.co.uk>
2024-06-13 16:54:28 +0100

    Add SDK docs button to settings

---
+++ LSDR/Assets/Scripts/UI/Title/UISDKButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace LSDR.UI.Title
+{
+    public class UISDKButton : MonoBehaviour, IPointerClickHandler
+    {
+        private readonly string KOFI_URL = "https://sdk.lsdrevamped.net";
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Application.OpenURL(KOFI_URL);
+        }
+    }
+}
